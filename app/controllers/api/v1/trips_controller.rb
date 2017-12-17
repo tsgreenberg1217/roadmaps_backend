16ee1468479b82a67b0f931402081fe5352bacfc
tsgreenberg1217 <tsgreenberg1217@gmail.com>
2017-12-16 20:59:42 -0500

    made trips show route

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::TripsController < ApplicationController
 
+  def show
+    #
+  end
+
   def create
     user = User.find(current_user.id)
     user.trips.create(title: params[:value])
