7fc4e89ee53a2878d15f6a7553ee1b34ba6e2728
tsgreenberg1217 <tsgreenberg1217@gmail.com>
2017-12-18 17:30:37 -0500

    made delete function

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -12,5 +12,12 @@ class Api::V1::TripsController < ApplicationController
     render json: user
   end
 
+  def destroy
+    user = User.find(current_user.id)
+    trip = user.trips.find_by(id: params[:id])
+    trip.destroy
+    render json: user
+  end
+
 
 end
