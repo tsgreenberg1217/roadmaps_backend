17b33838e37e7e11646f10b6c658960472d30537
tsgreenberg1217 <tsgreenberg1217@gmail.com>
2017-12-18 10:46:11 -0500

    going to add more tables

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -1,7 +1,9 @@
 class Api::V1::TripsController < ApplicationController
 
   def show
-    #
+    trip_id = params[:id].to_i
+    trip = current_user.trips.find_by(id: trip_id)
+    render json: trip
   end
 
   def create
--- app/serializers/trip_serializer.rb
+++ app/serializers/trip_serializer.rb
@@ -1,3 +1,3 @@
 class TripSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :title
 end
