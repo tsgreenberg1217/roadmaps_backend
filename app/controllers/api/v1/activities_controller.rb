05118800e7d56aed0fc4ff7535c921b9302941b4
tsgreenberg1217 <tsgreenberg1217@gmail.com>
2018-01-02 22:05:57 -0500

    progress

--- app/controllers/api/v1/activities_controller.rb
+++ app/controllers/api/v1/activities_controller.rb
@@ -1,9 +1,18 @@
+require 'pry'
 class Api::V1::ActivitiesController < ApplicationController
 
   def create
     stop = Stop.find(params[:stop_id])
     stop.activities.create(name: params[:activity])
     activities = stop.activities.all
+    render json: {stop: stop, activities: activities.map {|activity|  ActivitySerializer.new(activity)}}
+  end
+
+  def destroy
+    activity = Activity.find(params[:activity_id])
+    stop = Stop.find(activity.stop_id)
+    activity.destroy
+    activities = stop.activities
     render json: {stop: stop, activities: activities.map {|activity|  ActivitySerializer.new(activity)}}
   end
 
