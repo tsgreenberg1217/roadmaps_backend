f608401b7cfd1f2784800072367fa220f60935cc
tsgreenberg1217 <tsgreenberg1217@gmail.com>
2018-01-15 23:42:58 -0500

    local routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::API
   before_action :authorized
 
   def issue_token(payload)
-    # binding.pry
     JWT.encode(payload, ENV['SECRET'])
   end
 
@@ -13,17 +12,13 @@ class ApplicationController < ActionController::API
     # binding.pry
     authenticate_or_request_with_http_token do |jwt_token, options|
       begin
-        # binding.pry
-        ## I get the correct token but it keeps decoding to a differnt user
         decoded_token = JWT.decode(jwt_token, ENV['SECRET'])
 
       rescue JWT::DecodeError
-        # binding.pry
         return nil
       end
 
       if decoded_token[0]["user_id"]
-        # binding.pry
         @current_user ||= User.find(decoded_token[0]["user_id"])
       else
       end
@@ -31,12 +26,10 @@ class ApplicationController < ActionController::API
   end
 
   def logged_in?
-    # binding.pry
     !!current_user
   end
 
   def authorized
-    # binding.pry
     render json: {message: "Not welcome" }, status: 401 unless logged_in?
   end
 
