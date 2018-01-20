c5bd9bd8d58c7e964fa8defad5167bc3076c48b6
tsgreenberg1217 <tsgreenberg1217@gmail.com>
2018-01-20 13:48:39 -0500

    ready to format data for frontend

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -1,6 +1,8 @@
+require_relative '../../../helpers/GoogleAPI'
 require 'pry'
 
 class Api::V1::TripsController < ApplicationController
+  extend GoogleAPI
 
   def everytrip
     render json: Trip.all
@@ -22,9 +24,22 @@ class Api::V1::TripsController < ApplicationController
   end
 
   def create
+    binding.pry
     user = User.find(current_user.id)
-    user.trips.create(title: params[:value][:trip], photo: params[:value][:fileURL])
-    render json: user.trips
+    user.trips.create(title: params[:trip], photo: params[:fileURL])
+    trip = user.trips.last
+    start_location = GoogleAPI.coordinates(params[:start])
+    end_location = GoogleAPI.coordinates(params[:end])
+
+    trip.stops.create(name: params[:start], order:1, lat: start_location["lat"], lng: start_location["lng"])
+    trip.stops.create(name: params[:end], order:1, lat: end_location["lat"], lng: end_location["lng"])
+
+    stops = trip.stops.all.sort_by{|stop| stop.order}
+    Sorter.recount(stops) #Sorts the stops by order
+    stops = GoogleAPI.getDurations(stops) #gets and updates duration attributes
+    user_trips = user.trips
+    binding.pry
+    render json: {trips: user_trips.map {|trip| TripSerializer.new(trip)}, stops: stops.map{|stop| StopSerializer.new(stop)}}
   end
 
   def destroy
