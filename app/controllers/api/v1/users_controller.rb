56f9594e7d7ae40eb597dbaadcdffacab719cae9
tsgreenberg1217 <tsgreenberg1217@gmail.com>
2018-02-08 20:31:09 -0500

    configured for local

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -15,7 +15,6 @@ class Api::V1::UsersController < ApplicationController
       token = issue_token(payload)
       render json: {user: user, jwt: token}
     else
-      # binding.pry
       render json: {message: 'error with signup'}
     end
   end
@@ -24,7 +23,7 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :password, :email)
+    params.require(:user).permit(:name, :email)
   end
 
 end
